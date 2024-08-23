1a02b590b41a59d9d273bf639dbab7435ae49b05
chaitany mishra <mr.chetan105@gmail.com>
2024-08-23 20:57:49 +0530

    Fine managment systme using if/else  Operators  in python

--- fineSystme.py
+++ fineSystme.py
@@ -4,14 +4,14 @@
 # if attendenc (85 > 90 (500rs /-)
 # if attendenc (80 > 85 )(1000rs /-)
 
-attendec = int(input("ENTER STUDENT ATTENDENCE IN '%' "))
-if(attendec < 0 or attendec > 100):
+attendence = int(input("ENTER STUDENT ATTENDENCE IN '%' "))
+if(attendence < 0 or attendec > 100):
     print("INVALID INPUT! attendence must be between '0' and '100' !")
-elif(attendec >= 90):
+elif(attendence >= 90):
     print("NO FINE ! ")
-elif( 85 <= attendec  < 90):
+elif( 85 <= attendence  < 90):
     print("500rs/- fine will be awarded ! ")
-elif(  80 <= attendec < 85):
+elif(  80 <= attendence < 85):
     print("1000rs/- fine will be awarded ! ")
 else:
     print("NOT DECIDED YET! PLEASE CONTACT TO YOUR CO-ORDINATIOR ! ")
