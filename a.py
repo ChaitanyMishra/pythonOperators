3bbc4ae69315440f9266ed54e2db593d3a19cb00
chaitany mishra <mr.chetan105@gmail.com>
2024-09-10 19:26:37 +0530

    python mini project

--- a.py
+++ a.py
@@ -1,38 +1,31 @@
-attendance = int(input("ENTER STUDENT ATTENDANCE IN '%' "))
-
-if attendance < 0 or attendance > 100:
-    print("INVALID INPUT! Attendance must be between '0' and '100'!")
-else:
-    # Handle cases for attendance below 70
-    if attendance < 70:
-        details = input("If you have any medical issue type 'YES' else type 'NO': ").lower()
-        if details == "yes":
-            days = int(input("How many days were you sick? "))
-            # Adjust attendance based on medical days
-            new_attendance = days * 7  # assuming 7% attendance per sick day
-            adjusted_attendance = attendance + new_attendance
-            if adjusted_attendance > 100:
-                adjusted_attendance = 100  # Cap the attendance at 100%
-            attendance = adjusted_attendance
-
-    # Check fine status after attendance adjustments (if any)
-    if attendance >= 90:
-        print("NO FINE! You are eligible for the prize distribution policy!")
-    elif 85 <= attendance < 90:
-        print("500rs/- fine will be imposed.")
-    elif 80 <= attendance < 85:
-        print("You have to submit 1000rs/- fine + 1 assignment.")
-    elif 75 <= attendance < 80:
-        print("You have to submit 1500rs + 2 assignments for every subject.")
-    elif 70 <= attendance < 75:
-        print("You have to submit 2000rs fine + 3 assignments for every subject.")
-    elif 65 <= attendance < 70:
-        print("You have to submit 2500rs + 4 assignments for every subject.")
-    elif 60 <= attendance < 65:
-        print("3000rs + 4 assignments for every subject.")
-    elif 55 <= attendance < 60:
-        print("You have to submit 3500rs + 5 assignments for every subject.")
-    elif 50 <= attendance < 55:
-        print("You have to submit 4000rs + 6 assignments for every subject.")
-    else:
-        print("Submit the fine to the Co-Ordinator, arigatto!")
+size = int(input("Enter size of loop: "))
+print("Enter '1' for assending sort '2' for desending")
+choice = int (input("Enter your choice: "))
+if choice == 1:
+    num_1 = int(input("Index 1: "))
+    for i in range(2 , size+1):
+        num = int(input(f"Index {i}: "))
+        num = num_1
+        temp = num
+        if num > temp:
+            temp_2 = num
+            temp = temp_2
+        else:
+            continue
+    for i in range(1 , size+1):
+        print(num , end = " ")
+elif choice == 2:
+    num_1 = int(input("Index 1: "))
+    for i in range(2 , size+1):
+        num = int(input(f"Index {i}: "))
+        num = num_1
+        temp = num
+        if num < temp:
+            temp_2 = num
+            temp = temp_2
+        else:
+            continue
+for i in range(1 , size+1):
+    print(num , end = " ")
+      
+    
---
+++ pythonBasics.py
@@ -0,0 +1,2 @@
+#find area
+area = int()
