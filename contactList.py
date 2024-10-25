3c93daa9209b50fb887a93a70f33e4f4180b4623
chaitany mishra <mr.chetan105@gmail.com>
2024-10-25 22:33:05 +0530

    updated contact program

--- contactList.py
+++ contactList.py
@@ -1,26 +1,33 @@
 contact_list = {}
 while True:
-    print("\nOptions:")
+    print("\n***Main Menu***\n")
     print("1. Add Contact")
     print("2. Display Contacts")
     print("3. Edit contacts")
     print("4. Delete contacts")
-    print("5. Exit")
-    choice = input("Choose an option (1/2/3): ")
+    print("5. Search Contacts")
+    print("6. Sort Your Contacts")
+    print("7. Exit")
+    choice = input("Enter  your choice: ")
+    print("\n")
+
     if choice == '1':
         name = input("Enter Name: ")
+        if name in contact_list:
+            print("Contact already exists")
+            print("Enter Unique Name: ")
+            name = input("Enter Name: ")
         phone = input("Enter Phone Number: ")
         age = input("Enter Age: ")
         email = input("Enter Email Address: ")
         contact_list[name] = {'phone': phone, 'age': age, 'email': email}
-        print("Contact added successfully!")
-
+        print("Contact added successfully!") 
     elif choice == '2':
         if not contact_list:
             print("No contacts available.")
         else:
-            print("\nContact List:")
-            print(contact_list)
+            for name, details in contact_list.items():
+                print(f" Name: {name}\n Phone Number: {details['phone']}\n Age:  {details['age']}\n Email-> {details['email']}")
     elif choice == '3':
         name = input("Enter the name you want to edit: ")
         if name in contact_list:
@@ -30,8 +37,26 @@ while True:
             u_email = input("Enter new Email Address (leave blank to keep current): ")
         contact_list[name] = {'phone' : u_phone , 'age' : u_age , 'email' : u_email}
         print("Contact edited successfully!")
-            
+    elif choice == '4':
+        name = input("Enter the name you want to delete: ")
+        if name in contact_list:
+            del contact_list[name]
+            print(f"{name} Deleted sucessfuly!")
+        else:
+            print(f"{name} not found!")
     elif choice == '5':
+        name = input("Enter the name you want to search: ")
+        if name in contact_list:
+            print(f"Name: {name}")
+            print(f"Phone Number: {contact_list[name]['phone']}")
+            print("Exiting the program...")
+        else:
+            print(f"{name} not found!")
+    elif choice == '6':
+        print("Sorting contacts...")
+        sorted_dict = dict(sorted(contact_list.items()))
+        print(sorted_dict ,"\n")
+    elif choice == '7':
         print("Exiting the program.")
         break
 
